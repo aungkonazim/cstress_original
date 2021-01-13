0cfbe4b03dd4f5146d69e781bedc8d5c17aec3ec
aungkon <aungkonazim@gmail.com>
2021-01-13 14:20:58 -0600

    trained ecg only model

--- ecg.py
+++ ecg.py
@@ -22,126 +22,66 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from typing import List
-
 import numpy as np
 import scipy.signal as signal
 import datetime
+from scipy.stats import iqr
+import numpy as np
+from scipy.stats import iqr
+from scipy import interpolate, signal
+import matplotlib.pyplot as plt
+from matplotlib import style
+import matplotlib.patches as mpatches
+from collections import OrderedDict
+
+def frequencyDomain(tmStamps,RRints, lf_bw = 0.11, hf_bw = 0.1):
+    
+    #Remove ectopic beats
+    #RR intervals differing by more than 20% from the one proceeding it are removed
+    NNs = []
+    tss = []
+    for c, rr in enumerate(RRints):        
+        if abs(rr - RRints[c-1]) <= 0.20 * RRints[c-1]:
+            NNs.append(rr)
+            tss.append(tmStamps[c])
+            
+            
+    frequency_range = np.linspace(0.001, 1, 10000)
+    NNs = np.array(NNs)
+    NNs = NNs - np.mean(NNs)
+    result = signal.lombscargle(tss, NNs, frequency_range)
+        
+    #Pwelch w/ zero pad     
+    fxx = frequency_range 
+    pxx = result 
+    
+    vlf= (0.003, 0.04)
+    lf = (0.04, 0.15)
+    hf = (0.15, 0.4)
+    
+    plot_labels = ['VLF', 'LF', 'HF']
+        
+    df = fxx[1] - fxx[0]
+    vlf_power = np.trapz(pxx[np.logical_and(fxx >= vlf[0], fxx < vlf[1])], dx = df)      
+    lf_power = np.trapz(pxx[np.logical_and(fxx >= lf[0], fxx < lf[1])], dx = df)            
+    hf_power = np.trapz(pxx[np.logical_and(fxx >= hf[0], fxx < hf[1])], dx = df)             
+    totalPower = vlf_power + lf_power + hf_power
+    
+    #Normalize and take log
+    vlf_NU_log = np.log((vlf_power / (totalPower - vlf_power)) + 1)
+    lf_NU_log = np.log((lf_power / (totalPower - vlf_power)) + 1)
+    hf_NU_log = np.log((hf_power / (totalPower - vlf_power)) + 1)
+    lfhfRation_log = np.log((lf_power / hf_power) + 1)   
+    
+    freqDomainFeats = {'VLF_Power': vlf_NU_log, 'LF_Power': lf_NU_log,
+                       'HF_Power': hf_NU_log, 'LF/HF': lfhfRation_log}
+                       
+    return freqDomainFeats
 
 
 
-def lomb(time_stamps:List,
-         samples:List,
-         low_frequency: float,
-         high_frequency: float):
-    """
-   : Lomb–Scargle periodogram implementation
-    :param data: List[DataPoint]
-    :param high_frequency: float
-    :param low_frequency: float
-    :return lomb-scargle pgram and frequency values
-    """
-
-    frequency_range = np.linspace(low_frequency, high_frequency, len(time_stamps))
-    result = signal.lombscargle(time_stamps, samples, frequency_range)
-    return result, frequency_range
+def ecg_feature_computation(b,a):
+    return [np.var(a),iqr(a),np.mean(a),np.median(a),np.percentile(a,80),np.percentile(a,20),60000/np.median(a)]+list(frequencyDomain(b,a).values())
 
 
-def heart_rate_power(power: np.ndarray,
-                     frequency: np.ndarray,
-                     low_rate: float,
-                     high_rate: float):
-    """
-    Compute Heart Rate Power for specific frequency range
-    :param power: np.ndarray
-    :param frequency: np.ndarray
-    :param high_rate: float
-    :param low_rate: float
-    :return: sum of power for the frequency range
-    """
-    result_power = float(0.0)
-    for i, value in enumerate(power):
-        if low_rate <= frequency[i] <= high_rate:
-            result_power += value
-    return result_power
-
-
-
-
-
-
-def ecg_feature_computation(timestamp:list,
-                            value:list,
-                            low_frequency: float = 0.01,
-                            high_frequency: float = 0.7,
-                            low_rate_vlf: float = 0.0009,
-                            high_rate_vlf: float = 0.04,
-                            low_rate_hf: float = 0.15,
-                            high_rate_hf: float = 0.4,
-                            low_rate_lf: float = 0.04,
-                            high_rate_lf: float = 0.15):
-    """
-    ECG Feature Implementation. The frequency ranges for High, Low and Very low heart rate variability values are
-    derived from the following paper:
-    'Heart rate variability: standards of measurement, physiological interpretation and clinical use'
-    :param high_rate_lf: float
-    :param low_rate_lf: float
-    :param high_rate_hf: float
-    :param low_rate_hf: float
-    :param high_rate_vlf: float
-    :param low_rate_vlf: float
-    :param high_frequency: float
-    :param low_frequency: float
-    :param datastream: DataStream
-    :param window_size: float
-    :param window_offset: float
-    :return: ECG Feature DataStreams
-    """
-
-
-    # perform windowing of datastream
-
-
-    # initialize each ecg feature array
-
-    rr_variance_data = []
-    rr_mean_data = []
-    rr_median_data = []
-    rr_80percentile_data = []
-    rr_20percentile_data = []
-    rr_quartile_deviation_data = []
-    rr_HF_data = []
-    rr_LF_data = []
-    rr_VLF_data = []
-    rr_LF_HF_data = []
-    rr_heart_rate_data = []
-
-    # iterate over each window and calculate features
-
-
-    reference_data = value
-
-    rr_variance_data.append(np.var(reference_data))
-
-    power, frequency = lomb(time_stamps=timestamp,samples=value,low_frequency=low_frequency, high_frequency=high_frequency)
-
-    rr_VLF_data.append(heart_rate_power(power, frequency, low_rate_vlf, high_rate_vlf))
-
-    rr_HF_data.append(heart_rate_power(power, frequency, low_rate_hf, high_rate_hf))
-
-    rr_LF_data.append(heart_rate_power(power,frequency,low_rate_lf,high_rate_lf))
-
-    if heart_rate_power(power, frequency, low_rate_hf, high_rate_hf) != 0:
-        lf_hf = float(heart_rate_power(power, frequency, low_rate_lf, high_rate_lf) / heart_rate_power(power,
-                                                                                                       frequency,
-                                                                                                       low_rate_hf,
-                                                                                                       high_rate_hf))
-        rr_LF_HF_data.append(lf_hf)
-    else:
-        rr_LF_HF_data.append(0)
-
-    rr_mean_data.append(np.mean(reference_data))
-    rr_median_data.append(np.median(reference_data))
-    rr_quartile_deviation_data.append((0.5*(np.percentile(reference_data, 75) - np.percentile(reference_data,25))))
-    rr_heart_rate_data.append(60000/np.median(reference_data))
-    return [rr_variance_data[0],  rr_quartile_deviation_data[0], rr_VLF_data[0], rr_LF_data[0],rr_HF_data[0], rr_LF_HF_data[0],rr_mean_data[0],rr_median_data[0],np.percentile(value,80),np.percentile(value,20),rr_heart_rate_data[0]]
 
